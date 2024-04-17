f9aeced6a91dc1ea6730154fb2a9cf10b7f77e9f
ocoropuj <ocorom@uw.edu>
2024-04-17 13:11:08 -0700

    update

--- Round 3/trader_MM.py
+++ Round 3/trader_MM.py
@@ -39,43 +39,88 @@ class Trader:
             best_bid_S, best_bid_amount_S = list(order_depth_S.buy_orders.items())[0]
             best_bid_R, best_bid_amount_R = list(order_depth_R.buy_orders.items())[0]
             
-        ratio_short = 0.99
-        ratio_long = 1.01
+        ratio_long = 1.001
+        ratio_short = 0.999
 
-        acceptable_price_long_GB = 4 * best_ask_C + 6 * best_ask_S + best_ask_R
-        acceptable_price_short_GB = 4 * best_bid_C + 6 * best_bid_S + best_bid_R
-        if best_ask_GB > ratio_short * acceptable_price_short_GB:
-            orders_GB.append(Order('GIFT_BASKET', best_ask_GB, -best_ask_amount_GB))
-        if best_bid_GB < ratio_long * acceptable_price_long_GB:
-            orders_GB.append(Order('GIFT_BASKET', best_bid_GB, -best_bid_amount_GB))
-        result['GIFT_BASKET'] = orders_GB
+        B_GB = 1
+        B_C = 4
+        B_S = 6
+        B_R = 1
+
+        acceptable_price_long_GB = (B_C * best_ask_C + B_S * best_ask_S + B_R * best_ask_R) / B_GB
+        acceptable_price_short_GB = (B_C * best_bid_C + B_S * best_bid_S + B_R * best_bid_R) / B_GB 
+
+        acceptable_price_long_C = (B_GB * best_ask_GB - B_S * best_ask_S - B_R * best_ask_R) / B_C
+        acceptable_price_short_C = (B_GB * best_bid_GB - B_S * best_bid_S - B_R * best_bid_R) / B_C
+
+        acceptable_price_long_S = (B_GB * best_ask_GB - B_C * best_ask_C - B_R * best_ask_R) / B_S
+        acceptable_price_short_S = (B_GB * best_bid_GB - B_C * best_bid_C - B_R * best_bid_R) / B_S
+
+        acceptable_price_long_R = (B_GB * best_ask_GB - B_C * best_ask_C - B_S * best_ask_S) / B_R
+        acceptable_price_short_R = (B_GB * best_bid_GB - B_C * best_bid_C - B_S * best_bid_S) / B_R
+
+
+        if best_ask_GB < ratio_short * acceptable_price_short_GB:
+            orders_GB.append(Order('GIFT_BASKET', best_ask_GB, -best_ask_amount_GB)) # I Buy Gift Basket
+            orders_C.append(Order('CHOCOLATE', best_bid_C, - int(B_C * best_bid_amount_GB / B_GB))) # I Sell 4 chocolates
+            orders_S.append(Order('STRAWBERRIES', best_bid_S, - int(B_S * best_bid_amount_GB / B_GB))) # I Sell 6 strawberries
+            orders_R.append(Order('ROSES', best_bid_R, - int(B_R * best_bid_amount_GB / B_GB))) # I Sell 1 rose
+
+        if best_bid_GB > ratio_long * acceptable_price_long_GB:
+            orders_GB.append(Order('GIFT_BASKET', best_bid_GB, -best_bid_amount_GB)) # I Sell Gift Basket
+            orders_C.append(Order('CHOCOLATE', best_ask_C, - int(B_C * best_ask_amount_GB / B_GB))) # I Buy 4 chocolates
+            orders_S.append(Order('STRAWBERRIES', best_ask_S, - int(B_S * best_ask_amount_GB / B_GB))) # I Buy 6 strawberries
+            orders_R.append(Order('ROSES', best_ask_R, - int(B_R * best_ask_amount_GB / B_GB))) # I Buy 1 rose
+
+
+
+        if best_ask_C < ratio_short * acceptable_price_short_C:
+            orders_C.append(Order('CHOCOLATE', best_ask_C, -best_ask_amount_C)) # I Buy Chocolate
+            orders_GB.append(Order('GIFT_BASKET', best_bid_GB, - int(B_GB * best_bid_amount_C / B_C))) # I Sell Gift Basket
+            orders_S.append(Order('STRAWBERRIES', best_ask_S, - int(B_S * best_ask_amount_C / B_C))) # I Buy 6 strawberries
+            orders_R.append(Order('ROSES', best_ask_R, - int(B_R * best_ask_amount_C / B_C))) # I Buy 1 rose
         
-        acceptable_price_long_C = 0.25 * (best_ask_GB - 6 * best_ask_S - best_ask_R)
-        acceptable_price_short_C = 0.25 * (best_bid_GB - 6 * best_bid_S - best_bid_R)
-        if best_ask_C > ratio_short * acceptable_price_short_C:
-            orders_C.append(Order('CHOCOLATE', best_ask_C, -best_ask_amount_C))
-        if best_bid_C < ratio_long * acceptable_price_long_C:
-            orders_C.append(Order('CHOCOLATE', best_bid_C, -best_bid_amount_C))
+        if best_bid_C > ratio_long * acceptable_price_long_C:
+            orders_C.append(Order('CHOCOLATE', best_bid_C, -best_bid_amount_C)) # I Sell Chocolate
+            orders_GB.append(Order('GIFT_BASKET', best_ask_GB, - int(B_GB * best_ask_amount_C / B_C))) # I Buy Gift Basket
+            orders_S.append(Order('STRAWBERRIES', best_bid_S, - int(B_S * best_bid_amount_C / B_C))) # I Sell 6 strawberries
+            orders_R.append(Order('ROSES', best_bid_R, - int(B_R * best_bid_amount_C / B_C))) # I Sell 1 rose
+        
+
+
+        if best_ask_S < ratio_short * acceptable_price_short_S:
+            orders_S.append(Order('STRAWBERRIES', best_ask_S, -best_ask_amount_S)) # I Buy Strawberries
+            orders_GB.append(Order('GIFT_BASKET', best_bid_GB, - int(B_GB * best_bid_amount_S / B_S))) # I Sell Gift Basket
+            orders_C.append(Order('CHOCOLATE', best_ask_C, - int(B_C * best_ask_amount_S / B_S))) # I Buy 4 chocolates
+            orders_R.append(Order('ROSES', best_ask_R, - int(B_R * best_ask_amount_S / B_S))) # I Buy 1 rose
+        
+        if best_bid_S > ratio_long * acceptable_price_long_S:
+            orders_S.append(Order('STRAWBERRIES', best_bid_S, -best_bid_amount_S)) # I Sell Strawberries
+            orders_GB.append(Order('GIFT_BASKET', best_ask_GB, - int(B_GB * best_ask_amount_S / B_S))) # I Buy Gift Basket
+            orders_C.append(Order('CHOCOLATE', best_bid_C, - int(B_C * best_bid_amount_S / B_S))) # I Sell 4 chocolates
+            orders_R.append(Order('ROSES', best_bid_R, - int(B_R * best_bid_amount_S / B_S))) # I Sell 1 rose
+
+
+
+        if best_ask_R < ratio_short * acceptable_price_short_R:
+            orders_R.append(Order('ROSES', best_ask_R, -best_ask_amount_R)) # I Buy Roses
+            orders_GB.append(Order('GIFT_BASKET', best_bid_GB, - int(B_GB * best_bid_amount_R / B_R))) # I Sell Gift Basket
+            orders_C.append(Order('CHOCOLATE', best_ask_C, - int(B_C * best_ask_amount_R / B_R))) # I Buy 4 chocolates
+            orders_S.append(Order('STRAWBERRIES', best_ask_S, - int(B_S * best_ask_amount_R / B_R))) # I Buy 6 strawberries
+        
+        if best_bid_R > ratio_long * acceptable_price_long_R:
+            orders_R.append(Order('ROSES', best_bid_R, -best_bid_amount_R)) # I Sell Roses
+            orders_GB.append(Order('GIFT_BASKET', best_ask_GB, - int(B_GB * best_ask_amount_R / B_R))) # I Buy Gift Basket
+            orders_C.append(Order('CHOCOLATE', best_bid_C, - int(B_C * best_bid_amount_R / B_R))) # I Sell 4 chocolates
+            orders_S.append(Order('STRAWBERRIES', best_bid_S, - int(B_S * best_bid_amount_R / B_R))) # I Sell 6 strawberries
+
+
+        result['GIFT_BASKET'] = orders_GB
         result['CHOCOLATE'] = orders_C
-
-        acceptable_price_long_S = (best_ask_GB - 4 * best_ask_C - best_ask_R) / 6
-        acceptable_price_short_S = (best_bid_GB - 4 * best_bid_C - best_bid_R) / 6
-        if best_ask_S > ratio_short * acceptable_price_short_S:
-            orders_S.append(Order('STRAWBERRIES', best_ask_S, -best_ask_amount_S))
-        if best_bid_S < ratio_long * acceptable_price_long_S:
-            orders_S.append(Order('STRAWBERRIES', best_bid_S, -best_bid_amount_S))
         result['STRAWBERRIES'] = orders_S
-
-        acceptable_price_long_R = (best_ask_GB - 4 * best_ask_C - 6 * best_ask_S)
-        acceptable_price_short_R = (best_bid_GB - 4 * best_bid_C - 6 * best_bid_S)
-        if best_ask_R > ratio_short * acceptable_price_short_R:
-            orders_R.append(Order('ROSES', best_ask_R, -best_ask_amount_R))
-        if best_bid_R < ratio_long * acceptable_price_long_R:
-            orders_R.append(Order('ROSES', best_bid_R, -best_bid_amount_R))
         result['ROSES'] = orders_R
 
         traderData = 'sample'
 
 
         return result, 1, traderData
-    
