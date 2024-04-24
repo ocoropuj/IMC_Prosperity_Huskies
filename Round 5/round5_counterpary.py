faa483511be5d41ecc366111d279a2dc2d40a499
ocoropuj <ocorom@uw.edu>
2024-04-23 18:50:58 -0700

    table created

--- Round 5/round5_counterpary.py
+++ Round 5/round5_counterpary.py
@@ -3,6 +3,28 @@ from typing import List
 import string
 import numpy as np
 
+
+# Define traders and assets in the specified order
+traders = ['Adam', 'Amelia', 'Raj', 'Remy', 'Rhianna', 'Ruby', 'Valentina', 'Vinnie', 'Vladimir']
+assets = ['AMETHYSTS', 'STARFRUIT', 'GIFT_BASKET', 'CHOCOLATE', 'STRAWBERRIES', 'ROSES', 'COCONUT', 'COCONUT_COUPON']
+
+# Define the data
+data = {
+    'Adam': [0, 0, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+    'Amelia': [1, None, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+    'Raj': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, None, np.nan],
+    'Remy': [-1, -2, None, None, None, None, np.nan, np.nan],
+    'Rihana': [0, None, None, 0, np.nan, np.nan, -1, -2],
+    'Ruby': [-2, 0, None, np.nan, np.nan, np.nan, np.nan, None],
+    'Valentina': [0, None, np.nan, np.nan, np.nan, np.nan, np.nan, 3],
+    'Vinnie': [1, -2, 1.5, 2, 2.5, 2, -3, -3],
+    'Vladimir': [1, -1, 2, -3, -3, -2, np.nan, 2]
+}
+
+# Create the DataFrame
+df = pd.DataFrame(data, index=assets)
+
+
 class Trader:
     def __innit__(self):
           self.prices_starfruit = []
@@ -11,58 +33,32 @@ class Trader:
         print("traderData: " + state.traderData)
         print("Observations: " + str(state.observations))
 
-				# Orders to be placed on exchange matching engine
         result = {}
         for product in state.order_depths:
-            if product == 'AMETHYSTS':
-                  order_depth: OrderDepth = state.order_depths[product]
-                  own_trades: OwnTrade = 
-                  orders: List[Order] = []
-                  acceptable_price = 10000  # Participant should calculate this value
-                  print("Acceptable price : " + str(acceptable_price))
-                  print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+            order_depth: OrderDepth = state.order_depths[product]
+            own_trades: OwnTrade = state.own_trades[product]
+            orders: List[Order] = []
+            acceptable_price = 10000  # Participant should calculate this value
+            print("Acceptable price : " + str(acceptable_price))
+            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
       
-                  if len(order_depth.sell_orders) != 0:
-                        best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                        print(f'Best Ask: {best_ask_amount}')
-                        if int(best_ask) < acceptable_price:
-                              print("BUY", str(-best_ask_amount) + "x", best_ask)
-                              orders.append(Order(product, best_ask, -best_ask_amount))
-      
-                  if len(order_depth.buy_orders) != 0:
-                        best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                        print(f'Best bid: {best_bid_amount}')
-                        if int(best_bid) > acceptable_price:
-                              print("SELL", str(best_bid_amount) + "x", best_bid)
-                              orders.append(Order(product, best_bid, -best_bid_amount))
-                  
-                  result[product] = orders
-            else:
-                  order_depth: OrderDepth = state.order_depths[product]
-                  orders: List[Order] = []
-                  
-                  acceptable_price_sell = 5070  # Participant should calculate this value
-                  acceptable_price_buy = 5000
-                  print("Acceptable price : " + str(acceptable_price))
-                  print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-      
-                  if len(order_depth.sell_orders) != 0:
-                        best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                        print(f'Best Ask: {best_ask_amount}')
-                        if int(best_ask) < acceptable_price_buy:
-                              print("BUY", str(-best_ask_amount) + "x", best_ask)
+            if len(order_depth.sell_orders) != 0:
+                  best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                  print(f'Best Ask: {best_ask_amount}')
+                  if int(best_ask) < acceptable_price:
+                        print("BUY", str(-best_ask_amount) + "x", best_ask)
                         orders.append(Order(product, best_ask, -best_ask_amount))
       
-                  if len(order_depth.buy_orders) != 0:
-                        best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                        print(f'Best bid: {best_bid_amount}')
-                        if int(best_bid) > acceptable_price_sell:
-                              print("SELL", str(best_bid_amount) + "x", best_bid)
-                              orders.append(Order(product, best_bid, -best_bid_amount))
-                  
-                  result[product] = orders 
-		    # String value holding Trader state data required. 
-				# It will be delivered as TradingState.traderData on next execution.
+            if len(order_depth.buy_orders) != 0:
+                  best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                  print(f'Best bid: {best_bid_amount}')
+                  if int(best_bid) > acceptable_price:
+                        print("SELL", str(best_bid_amount) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -best_bid_amount))
+
+            result[product] = orders 
+		# String value holding Trader state data required. 
+  
         traderData = "SAMPLE" 
         
 				# Sample conversion request. Check more details below. 
